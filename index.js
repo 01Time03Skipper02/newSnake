abd0a030bd67e81dd6f4e45de71efdc301d765ab
San4ezzZ03 <sasha.shvech2003@mail.ru>
2022-03-13 03:01:48 +0300

    1 file modified

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const ctx = canvas.getContext('2d');
 
 const ground = new Image();
 ground.src =
-  'https://raw.githubusercontent.com/San4ezzZ03/newSnake/master/ground.png';
+  'https://raw.githubusercontent.com/San4ezzZ03/newSnake/master/img/ground.png';
 
 const foodImg = new Image();
 foodImg.src =
-  'https://raw.githubusercontent.com/San4ezzZ03/newSnake/master/food.png';
+  'https://stackblitz.com/files/js-nbutl7/github/San4ezzZ03/newSnake/master/food.png';
 
 let box = 32;
 
