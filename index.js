f6bc4c8a05d35ac827a3bb782ba94bdeb0abd958
San4ezzZ03 <sasha.shvech2003@mail.ru>
2022-03-13 03:15:04 +0300

    1 file modified

--- index.js
+++ index.js
@@ -32,10 +32,14 @@ document.addEventListener('keydown', direction);
 let dir;
 
 function direction(event) {
-  if (event.keyCode == 37 && dir != 'right') dir = 'left';
-  else if (event.keyCode == 38 && dir != 'down') dir = 'up';
-  else if (event.keyCode == 39 && dir != 'left') dir = 'right';
-  else if (event.keyCode == 40 && dir != 'up') dir = 'down';
+  if ((event.keyCode == 65 || event.keyCode == 37) && dir != 'right')
+    dir = 'left';
+  else if ((event.keyCode == 87 || event.keyCode == 38) && dir != 'down')
+    dir = 'up';
+  else if ((event.keyCode == 68 || event.keyCode == 39) && dir != 'left')
+    dir = 'right';
+  else if ((event.keyCode == 83 || event.keyCode == 40) && dir != 'up')
+    dir = 'down';
 }
 
 function eatTail(head, arr) {
